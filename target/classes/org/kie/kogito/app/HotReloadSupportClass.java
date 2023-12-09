5297fa38d56ba5e1724a4bde676976323d9e36c9
processmining123 <zhaoyq@cn.ibm.com>
2023-12-09 19:38:49 +0800

    good test

---
+++ target/classes/org/kie/kogito/app/HotReloadSupportClass.java
@@ -0,0 +1,5 @@
+package org.kie.kogito.app;
+@io.quarkus.runtime.Startup()
+public class HotReloadSupportClass {
+private static final String ID = "e0a39637-d56f-424d-b163-9a50e95c065f";
+}
---
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -0,0 +1,5 @@
+package org.kie.kogito.app;
+@io.quarkus.runtime.Startup()
+public class HotReloadSupportClass {
+private static final String ID = "5c7ac7b9-30b7-43b9-9f04-f0a9a1188e25";
+}
